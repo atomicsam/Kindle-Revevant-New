ff803a4cafe9498a1feed9a902105b875e71807e
atomicsam <61165057+atomicsam@users.noreply.github.com>
2024-09-09 22:45:27 +0100

    Initialised Qt Project

---
+++ KindleRevenant/ui_form.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+
+class Ui_Ui_KindleRevenant(object):
+    def setupUi(self, Ui_KindleRevenant):
+        if not Ui_KindleRevenant.objectName():
+            Ui_KindleRevenant.setObjectName(u"Ui_KindleRevenant")
+        Ui_KindleRevenant.resize(800, 600)
+
+        self.retranslateUi(Ui_KindleRevenant)
+
+        QMetaObject.connectSlotsByName(Ui_KindleRevenant)
+    # setupUi
+
+    def retranslateUi(self, Ui_KindleRevenant):
+        Ui_KindleRevenant.setWindowTitle(QCoreApplication.translate("Ui_KindleRevenant", u"Ui_KindleRevenant", None))
+    # retranslateUi
+
