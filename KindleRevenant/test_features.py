464115b0035657588d14aae373bd06786021ae4c
atomicsam <61165057+atomicsam@users.noreply.github.com>
2024-09-26 18:56:59 +0100

    created basic tests for application

---
+++ KindleRevenant/test_features.py
@@ -0,0 +1,45 @@
+import pytest
+
+from PySide6.QtSql import QSqlDatabase, QSqlQuery
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import (
+    QMainWindow,
+    QTableWidget,
+    QApplication,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QWidget,
+    QHBoxLayout,
+    QPushButton,
+    QHeaderView,
+    QFileDialog,
+    QAbstractItemView,
+    QToolBar,
+    QLabel,
+    QMessageBox,
+    QStatusBar,
+    QToolButton,
+    QMenu,
+    QWidgetAction,
+)
+
+import ui_kindlerevenant
+
+@pytest.fixture
+def getApp(qtbot):
+    test_ui = ui_kindlerevenant.Ui_KindleRevenant()
+    qtbot.addWidget(test_ui)
+
+    return test_ui
+
+def testDBConnectionClosedAfterStarup(getApp):
+    assert not hasattr(getApp, 'dbCon')
+
+def testOpenDatabase(getApp):
+    getApp.openDatabase()
+    assert getApp.dbCon.connectionName() in QSqlDatabase.connectionNames()
+
+def testDatabaseClose(getApp):
+    getApp.openDatabase()
+    getApp.closeDatabase()
+    assert not hasattr(getApp, 'dbCon')
